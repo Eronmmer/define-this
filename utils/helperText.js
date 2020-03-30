5b271d9efcaa84476f658314e6dc457bc7e87550
Erons <eronmmer@gmail.com>
2020-03-30 21:02:59 +0100

    indent helper text

--- utils/helperText.js
+++ utils/helperText.js
@@ -6,30 +6,28 @@ const magenta = chalk.magenta;
 
 const helperText = () => {
 	console.log(`
-	Usage
-	  ${green("define-this")} ${cyan("<command>")} ${yellow("[--option]")}
+Usage
+ ${green("define-this")} ${cyan("<command>")} ${yellow("[--option]")}
 
-	Commands
-	  ${cyan(
-			"define"
-		)}       Get the definition of a word(requires an argument: word)
-	  ${cyan(
-			"synonym"
-		)}      Get the synonym(s) of a word(optional argument: number)
+Commands
+ ${cyan(
+		"define"
+ )}       Get the definition of a word(requires an argument: word)
+ ${cyan("synonym")}      Get the synonym(s) of a word(optional argument: number)
 
-	Options
-	  ${yellow("--word")}, ${yellow(
+Options
+ ${yellow("--word")}, ${yellow(
 		"-w"
-	)}        The word whose synonym/definition would be displayed
-	  ${yellow("--number")}, ${yellow(
+	)}      The word whose synonym/definition would be displayed
+ ${yellow("--number")}, ${yellow(
 		"-n"
-	)}      Number of synonyms to display. Defaults to one if not supplied
+	)}    Number of synonyms to display. Defaults to one if not supplied
 
-	Examples
-	  ${green("define-this")} ${cyan("define")} ${yellow("-w")} ${magenta(
+Examples
+ ${green("define-this")} ${cyan("define")} ${yellow("-w")} ${magenta(
 		"phenomenon"
 	)}
-	  ${green("define-this")} ${cyan("synonym")} ${yellow("-w")} ${cyan(
+ ${green("define-this")} ${cyan("synonym")} ${yellow("-w")} ${cyan(
 		"flamboyant"
 	)} ${yellow("-n")} ${magenta("4")}
 `);
